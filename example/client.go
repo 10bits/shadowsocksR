b89a38c996c77183ae89687b64dee01b6686e89b
10bits <loseher2020@outlook.com>
2021-06-09 19:32:18 +0800

    update

--- example/client.go
+++ example/client.go
@@ -1,132 +1,103 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
 	"net"
-	"net/url"
+	"shadowsocksr"
+	ssr "shadowsocksr"
+	"shadowsocksr/tools/socks"
 	"time"
 
-	"shadowsocksr"
-	"shadowsocksr/tools/leakybuf"
-	"shadowsocksr/tools/socks"
+	"github.com/txthinking/socks5"
 )
 
-var (
-	readTimeout = 600 * time.Second
-)
+var bi *ssr.BackendInfo
 
-// SSInfo fields that shadowsocks/shadowsocksr used only
-type SSInfo struct {
-	SSRInfo
-	EncryptMethod   string
-	EncryptPassword string
+func init() {
+	ssr_url := "ssr://UE5FVU1PTk9VTFRSQU1JQ1JPU0NPUElDU0lMSUNPVk9MQ0FOT0NPTklPU0lTLnNhYzkwOTBzYWMuZ3E6MzE1NjM6YXV0aF9hZXMxMjhfbWQ1OmNoYWNoYTIwLWlldGY6dGxzMS4yX3RpY2tldF9hdXRoOlZqRm5VMkpoUzJ0NlFnLz9vYmZzcGFyYW09VUU1RlZVMVBUazlWVEZSU1FVMUpRMUpQVTBOUFVFbERVMGxNU1VOUFZrOU1RMEZPVDBOUFRrbFBVMGxUTG5OaFl6a3dPVEJ6WVdNdVozRSZwcm90b3BhcmFtPSZyZW1hcmtzPTVZV042TFM1TlEmZ3JvdXA9"
+	server := ssr.NewSSRServer(ssr_url, true)
+	if server != nil && server.BackendInfo != nil {
+		bi = server.BackendInfo
+	}
 }
 
-// SSRInfo fields that shadowsocksr used only
-type SSRInfo struct {
-	Obfs          string
-	ObfsParam     string
-	ObfsData      interface{}
-	Protocol      string
-	ProtocolParam string
-	ProtocolData  interface{}
+type handle struct{}
+
+func (this *handle) replyFailed(r *socks5.Request, w io.Writer) error {
+	var p *socks5.Reply
+	if r.Atyp == socks5.ATYPIPv4 || r.Atyp == socks5.ATYPDomain {
+		p = socks5.NewReply(socks5.RepHostUnreachable, socks5.ATYPIPv4, []byte{0x00, 0x00, 0x00, 0x00}, []byte{0x00, 0x00})
+	} else {
+		p = socks5.NewReply(socks5.RepHostUnreachable, socks5.ATYPIPv6, []byte(net.IPv6zero), []byte{0x00, 0x00})
+	}
+	if _, err := p.WriteTo(w); err != nil {
+		return err
+	}
+	return nil
 }
 
-// BackendInfo all fields that a backend used
-type BackendInfo struct {
-	SSInfo
-	Address string
-	Type    string
+func (this *handle) replySuccess(a byte, addr []byte, port []byte, w io.Writer) error {
+	p := socks5.NewReply(socks5.RepSuccess, a, addr, port)
+	if _, err := p.WriteTo(w); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (this *handle) TCPHandle(s *socks5.Server, localConn *net.TCPConn, req *socks5.Request) error {
+	if bi == nil {
+		return this.replyFailed(req, localConn)
+	}
+	rawaddr := socks.ParseAddr(req.Address())
+	tmp, err := bi.DialSSRConn(rawaddr)
+	if err != nil {
+		return this.replyFailed(req, localConn)
+	}
+	remoteConn := tmp.(*shadowsocksr.SSTCPConn)
+	a, addr, port, err := socks5.ParseAddress(remoteConn.LocalAddr().String())
+	if err != nil {
+		return this.replyFailed(req, localConn)
+	}
+	err = this.replySuccess(a, addr, port, localConn)
+	if err != nil {
+		return err
+	}
+	go PipeThenClose(localConn, remoteConn, nil)
+	PipeThenClose(remoteConn, localConn, nil)
+	return nil
+}
+func (this *handle) UDPHandle(s *socks5.Server, addr *net.UDPAddr, data *socks5.Datagram) error {
+	return nil
 }
 
 func main() {
-	bi := &BackendInfo{
-		Address: "www.domain.com:445",
-		Type:    "ssr",
-		SSInfo: SSInfo{
-			EncryptMethod:   "aes-128-cfb",
-			EncryptPassword: "password",
-			SSRInfo: SSRInfo{
-				Protocol:      "auth_aes128_sha1",
-				ProtocolParam: "",
-				Obfs:          "tls1.2_ticket_auth",
-				ObfsParam:     "",
-			},
-		},
+	s, err := socks5.NewClassicServer(":8080", "", "", "", 3000, 3000)
+	if err != nil {
+		log.Fatal(err)
 	}
-	bi.Listen()
+	s.ListenAndServe(&handle{})
 }
 
-func (bi *BackendInfo) Listen() {
-	listener, err := net.Listen("tcp", "0.0.0.0:2515")
-	if err != nil {
-		panic(err)
-	}
-	for {
-		localConn, err := listener.Accept()
-		if err != nil {
-			continue
-		}
-		go bi.Handle(localConn)
-	}
-}
+var readTimeout = time.Second * 10
 
-func (bi *BackendInfo) Handle(src net.Conn) {
-	//直接访问google
-	rawaddr := socks.ParseAddr("www.google.com.hk:443")
-
-	dst, err := bi.DialSSRConn(rawaddr)
-	if err != nil {
-		panic(err)
+func SetReadTimeout(c net.Conn) {
+	if readTimeout != 0 {
+		c.SetReadDeadline(time.Now().Add(readTimeout))
 	}
-	go bi.Pipe(src, dst)
-	bi.Pipe(dst, src)
-	src.Close()
-	dst.Close()
-}
-
-func (bi *BackendInfo) DialSSRConn(rawaddr socks.Addr) (net.Conn, error) {
-	u := &url.URL{
-		Scheme: bi.Type,
-		Host:   bi.Address,
-	}
-	v := u.Query()
-	v.Set("encrypt-method", bi.EncryptMethod)
-	v.Set("encrypt-key", bi.EncryptPassword)
-	v.Set("obfs", bi.Obfs)
-	v.Set("obfs-param", bi.ObfsParam)
-	v.Set("protocol", bi.Protocol)
-	v.Set("protocol-param", bi.ProtocolParam)
-	u.RawQuery = v.Encode()
-
-	ssrconn, err := shadowsocksr.NewSSRClient(u)
-	if err != nil {
-		return nil, fmt.Errorf("connecting to SSR server failed :%v", err)
-	}
-
-	if bi.ObfsData == nil {
-		bi.ObfsData = ssrconn.IObfs.GetData()
-	}
-	ssrconn.IObfs.SetData(bi.ObfsData)
-
-	if bi.ProtocolData == nil {
-		bi.ProtocolData = ssrconn.IProtocol.GetData()
-	}
-	ssrconn.IProtocol.SetData(bi.ProtocolData)
-
-	if _, err := ssrconn.Write(rawaddr); err != nil {
-		ssrconn.Close()
-		return nil, err
-	}
-	return ssrconn, nil
 }
 
 // PipeThenClose copies data from src to dst, closes dst when done.
-func (bi *BackendInfo) Pipe(src, dst net.Conn) error {
-	buf := leakybuf.GlobalLeakyBuf.Get()
+func PipeThenClose(src, dst net.Conn, addTraffic func(int)) {
+	defer dst.Close()
+	buf := leakyBuf.Get()
+	defer leakyBuf.Put(buf)
 	for {
-		src.SetReadDeadline(time.Now().Add(readTimeout))
+		SetReadTimeout(src)
 		n, err := src.Read(buf)
+		if addTraffic != nil {
+			addTraffic(n)
+		}
 		// read may return EOF with n > 0
 		// should always process n > 0 bytes before handling error
 		if n > 0 {
@@ -139,10 +110,56 @@ func (bi *BackendInfo) Pipe(src, dst net.Conn) error {
 			// Always "use of closed network connection", but no easy way to
 			// identify this specific error. So just leave the error along for now.
 			// More info here: https://code.google.com/p/go/issues/detail?id=4373
+			/*
+				if bool(Debug) && err != io.EOF {
+					Debug.Println("read:", err)
+				}
+			*/
 			break
 		}
 	}
-	leakybuf.GlobalLeakyBuf.Put(buf)
-	dst.Close()
-	return nil
+	return
+}
+
+type LeakyBuf struct {
+	bufSize  int // size of each buffer
+	freeList chan []byte
+}
+
+const leakyBufSize = 4108 // data.len(2) + hmacsha1(10) + data(4096)
+const maxNBuf = 2048
+
+var leakyBuf = NewLeakyBuf(maxNBuf, leakyBufSize)
+
+// NewLeakyBuf creates a leaky buffer which can hold at most n buffer, each
+// with bufSize bytes.
+func NewLeakyBuf(n, bufSize int) *LeakyBuf {
+	return &LeakyBuf{
+		bufSize:  bufSize,
+		freeList: make(chan []byte, n),
+	}
+}
+
+// Get returns a buffer from the leaky buffer or create a new buffer.
+func (lb *LeakyBuf) Get() (b []byte) {
+	select {
+	case b = <-lb.freeList:
+	default:
+		b = make([]byte, lb.bufSize)
+	}
+	return
+}
+
+// Put add the buffer into the free buffer pool for reuse. Panic if the buffer
+// size is not the same with the leaky buffer's. This is intended to expose
+// error usage of leaky buffer.
+func (lb *LeakyBuf) Put(b []byte) {
+	if len(b) != lb.bufSize {
+		panic("invalid buffer size that's put into leaky buffer")
+	}
+	select {
+	case lb.freeList <- b:
+	default:
+	}
+	return
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,10 @@ require (
 	github.com/dgryski/go-idea v0.0.0-20170306091226-d2fb45a411fb
 	github.com/dgryski/go-rc2 v0.0.0-20150621095337-8a9021637152
 	github.com/ebfe/rc2 v0.0.0-20131011165748-24b9757f5521 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/txthinking/runnergroup v0.0.0-20210608031112-152c7c4432bf // indirect
+	github.com/txthinking/socks5 v0.0.0-20210608030952-444f74aaf176
+	github.com/txthinking/x v0.0.0-20210326105829-476fab902fbe // indirect
 	gitlab.com/yawning/chacha20.git v0.0.0-20190903091407-6d1cb28dc72c
 	golang.org/x/crypto v0.0.0-20191122220453-ac88ee75c92c
 )
